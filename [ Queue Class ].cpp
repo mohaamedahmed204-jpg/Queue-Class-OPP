4bda60427ccd8c980e49cbf213587f7be6e753ae
Mohamed Ahmed Gwiada <mohaamedahmed204@gmail.com>
2026-08-12 00:20:49 +0300

    Update [ Queue Class ].cpp

--- [ Queue Class ].cpp
+++ [ Queue Class ].cpp
@@ -3,15 +3,14 @@
 
 using namespace std;
 
-int main()
-{
-    clsMyQueue <short> MyQueue;
+int main() {
+    clsMyQueue <int> MyQueue;
 
-    MyQueue.Push(10);
-    MyQueue.Push(20);
-    MyQueue.Push(30);
-    MyQueue.Push(40);
-    MyQueue.Push(50);
+    MyQueue.Push(100);
+    MyQueue.Push(200);
+    MyQueue.Push(300);
+    MyQueue.Push(400);
+    MyQueue.Push(500);
 
     cout << "\nQueue :\n";
     MyQueue.Print();
@@ -49,7 +48,6 @@ int main()
     MyQueue.Clear();
     cout << "\n\Queue after Clear() :\n";
     MyQueue.Print();
-
-    system("pause>0");
+    
     return 0;
 }
